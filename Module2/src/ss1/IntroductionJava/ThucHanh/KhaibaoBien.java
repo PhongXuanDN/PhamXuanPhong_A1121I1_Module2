16678664905f361f496f4f75c0a6c25cacdeb94c
phamxuanphong238@gmail.com <phamxuanphong238@gmail.com>
2022-02-20 18:47:36 -0800

    Update thuc hanh ss1

---
+++ Module2/src/ss1/IntroductionJava/ThucHanh/KhaibaoBien.java
@@ -0,0 +1,13 @@
+package ss1.IntroductionJava.ThucHanh;
+
+public class KhaibaoBien {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 10.5f;
+        double d= 4.5;
+        boolean b = false;
+        char c = 'a';
+        String str = "Da Nang";
+        System.out.println("i ="+i+", f= "+f + ", d = "+d);
+    }
+}
---
+++ Module2/src/ss1/IntroductionJava/ThucHanh/LayTime.java
@@ -0,0 +1,11 @@
+package ss1.IntroductionJava.ThucHanh;
+
+import java.util.Date;
+
+public class LayTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: "+now);
+
+    }
+}
